39e3480fb4d7ce91d3664cf5fdee80a56c39c9f5
jonatan.leal <jonatan.leal@udea.edu.co>
2023-11-26 17:45:42 -0500

    footer fixed

--- target/generated-sources/annotations/com/udea/bookclub/domain/Comment_.java
+++ target/generated-sources/annotations/com/udea/bookclub/domain/Comment_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-11-24T13:35:25")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-11-26T16:59:28")
 @StaticMetamodel(Comment.class)
 public class Comment_ { 
 
--- target/generated-sources/annotations/com/udea/bookclub/domain/Discussion_.java
+++ target/generated-sources/annotations/com/udea/bookclub/domain/Discussion_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-11-24T13:35:25")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-11-26T16:59:28")
 @StaticMetamodel(Discussion.class)
 public class Discussion_ { 
 
